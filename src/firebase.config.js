3a2cbe0715e6e0965f55e77da05b0c0fc5b34a34
Jeremy Asirwaad <jeremyasirwaad@Jeremys-MacBook-Air.local>
2023-05-25 10:36:59 +0530

    done

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyA9J1T-gRYAN5dC6V9Nr-gaZG23TtbtX6E",
-	authDomain: "tngasa-otp.firebaseapp.com",
-	projectId: "tngasa-otp",
-	storageBucket: "tngasa-otp.appspot.com",
-	messagingSenderId: "758898665174",
-	appId: "1:758898665174:web:c9ae39ffe26ff482c88b77",
-	measurementId: "G-XZDZ1FV9RR"
+	apiKey: "AIzaSyCpbDjv0YIXj0H_Jv76L0mATPbAofvfzCQ",
+	authDomain: "jeremy-dcb6e.firebaseapp.com",
+	projectId: "jeremy-dcb6e",
+	storageBucket: "jeremy-dcb6e.appspot.com",
+	messagingSenderId: "869635396061",
+	appId: "1:869635396061:web:b1e45a2300d60b5c0d6634",
+	measurementId: "G-B8NZSLZ7JG"
 };
 
 // Initialize Firebase
